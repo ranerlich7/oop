70fa4f41f214288d47a85597366d160263545b4a
Ran Erlich <ranerlich@gmail.com>
2024-08-30 11:49:58 +0300

    added methods to car"

--- car/models.py
+++ car/models.py
@@ -1,16 +1,31 @@
 class Car:  
-    def __init__(self, model, number, color="Black", fuel=50):
+    def __init__(self, model, number, color="Black", fuel=50, full_tank=50):
         self.model = model
         self.number = number
         self.color = color
         self.fuel = fuel
+        self.full_tank = full_tank
         print("starting a new car")
 
     def __str__(self):
         return f'model:{self.model}'
 
+    # def drive(self, kilometer):
+    #     fuel_c = kilometer / 10
+    #     self.fuel = self.fuel - fuel_c
+
     def drive(self, fuel_consumption):
-        print("driving....fuel consumption:",fuel_consumption)
+        # print("driving....fuel consumption:",fuel_consumption)
+        if (self.fuel - fuel_consumption) < 0:
+            print("you cannot drive so far!!!")
+            return
         self.fuel = self.fuel - fuel_consumption
-        print("fuel left:", self.fuel)
-        
+        # print("fuel left:", self.fuel)
+
+    def fueling(self, fuel_amount=-1):
+        if fuel_amount == -1:
+            self.fuel_amount = self.full_tank
+        else:
+            self.fuel = self.fuel + fuel_amount
+        # print('After fueling. fule is', self.fuel)
+
--- car/tests.py
+++ car/tests.py
@@ -5,7 +5,14 @@ car2 = Car("Ford","222-555-66")
 
 car1.model = "Toyota Corolla"
 
-print(car1)
-car1.drive(5)
-car2.drive(10)
+car1.drive(15)
+print("fuel is:", car1.fuel)
+car1.drive(15)
+print("fuel is:", car1.fuel)
+car1.fueling(20)
+print("final fuel is:", car1.fuel)
+# print(car1)
+# car2.drive(10)
+
+car1.drive(10)
 
